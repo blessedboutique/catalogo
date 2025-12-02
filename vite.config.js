047a617a9c005e01c30c3908adc95429ab7cf873
Skynet <yhudyllanos@gmail.com>
2025-12-01 23:09:53 -0400

    Update config for new repository name blessedboutique

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/blessed-boutique/', // Cambia 'blessed-boutique' por el nombre de tu repositorio
+  base: '/blessedboutique/', // Base path para GitHub Pages
 })
